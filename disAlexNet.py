5441af0bdbdc3bbc8a2b8be8c1789f1952dd7162
AndyShen105 <31282895+AndyShen105@users.noreply.github.com>
2017-09-21 17:52:58 +0800

    Update disAlexNet.py

--- disAlexNet.py
+++ disAlexNet.py
@@ -42,7 +42,7 @@ tf.app.flags.DEFINE_string("optimizer", "SGD", "optimizer we adopted")
 tf.app.flags.DEFINE_integer("Batch_size", 128, "Batch size")
 tf.app.flags.DEFINE_float("Learning_rate", 0.0001, "Learning rate")
 tf.app.flags.DEFINE_integer("Epoch", 10, "Epoch")
-tf.app.flags.DEFINE_integer("imagenet_path", 10, "ImageNet data path")
+tf.app.flags.DEFINE_string("imagenet_path", 10, "ImageNet data path")
 FLAGS = tf.app.flags.FLAGS
 
 # start a server for a specific task
